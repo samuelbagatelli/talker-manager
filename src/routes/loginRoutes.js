3e0b6379fb708ff4768ef77be3a2a5c66a292ba9
Samuel Bagatelli <contato.samuelbagatelli@gmail.com>
2022-10-04 18:23:53 -0300

    Requisito 4

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -9,7 +9,16 @@ const generateToken = () => crypto.randomBytes(8).toString('hex');
 
 router.post('/', (req, res) => {
   const emailValidation = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-  if (emailValidation.test(req.body.email)) res.status(200).json({ token: generateToken() });
+  if (!req.body.email) res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  if (!emailValidation.test(req.body.email)) { 
+    res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' }); 
+  }
+  if (!req.body.password) res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  if (req.body.password.length >= 6) {
+    res.status(200).json({ token: generateToken() });
+  } else {
+    res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
 });
 
 module.exports = router;
